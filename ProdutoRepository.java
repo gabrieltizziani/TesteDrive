ab19ad1c8e7964429cb99f14296c21e15b4a98c8
gabrieltizziani <126525209+gabrieltizziani@users.noreply.github.com>
2024-03-15 19:01:25 -0300

    Delete ProdutoRepository.java

--- ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.DriveThru.DriveThru.repository;
-
-import com.DriveThru.DriveThru.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
-}
