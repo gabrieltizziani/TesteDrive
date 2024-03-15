a1fc17efe607e854aa1c989a0e00f00f4cecc7f2
gabrieltizziani <126525209+gabrieltizziani@users.noreply.github.com>
2024-03-15 20:14:38 -0300

    Delete DriveThru directory

--- DriveThru/src/main/java/com/DriveThru/DriveThru/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.DriveThru.DriveThru.repository;
-
-import com.DriveThru.DriveThru.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
-}
